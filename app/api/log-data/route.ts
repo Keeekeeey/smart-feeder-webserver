0c2d48feb87b74c68619c6e262a01d6fdf1e5eb2
keeekeeey <snowballintexas@gmail.com>
2026-01-26 16:48:13 -0600

    feat: POST requires API

---
+++ app/api/log-data/route.ts
@@ -0,0 +1,42 @@
+import prisma from '@/lib/prisma'
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(request :NextRequest) {
+    const apiKey = request.headers.get('authorization')?.replace('Bearer ','');
+
+    if (apiKey !== process.env.API_SECRET_KEY){
+        return NextResponse.json(
+        { error: 'Unauthorized - Invalid API key' },
+        { status: 401 }
+    )};
+    try{ 
+        const body = await request.json();
+        const { motion } = await request.json();
+        console.log('pkg', motion)
+
+        if (motion !== 'boolean'){
+            return NextResponse.json(
+                { error : 'Motion must be of type boolean'},
+                { status  : 400}
+            )
+        }
+
+        await prisma.motionEvent.create({
+                data: {
+                    motion: motion,
+                    timestamp: new Date()
+                }
+            });
+        
+        return NextResponse.json(
+            { success : true}, 
+            { status : 201 }
+        );
+    } catch (error) {
+    console.error('Error logging data:', error);
+    return NextResponse.json(
+      { error: 'Internal server error' },
+      { status: 500 }
+    );
+  }
+}
